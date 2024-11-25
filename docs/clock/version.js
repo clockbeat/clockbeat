ffa980b15bc8757dcb3b87305c078bdd18b27505
clockbeat <89096549+clockbeat@users.noreply.github.com>
2024-11-25 15:57:34 +0000

    Complete new version

--- docs/clock/version.js
+++ docs/clock/version.js
@@ -1,1 +1,3 @@
+let cacheName = /*time!*/ "67447073";
 
+let projectFiles = /*files!*/ ["index.html", "sw.js", "version.js", "js/clock.js", "js/moveit.js", "js/storage.js", "js/suntimes.js", "media/bong.mp3", "media/clock.svg", "media/tick.mp3"]

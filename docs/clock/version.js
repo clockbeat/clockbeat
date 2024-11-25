50110a5242941f83411728ac8c870f9adc6114a7
clockbeat <89096549+clockbeat@users.noreply.github.com>
2024-11-25 15:49:54 +0000

    Delete docs/clock directory

--- docs/clock/version.js
+++
@@ -1,3 +0,0 @@
-let cacheName = /*time!*/ "669e8274";
-
-let projectFiles = /*files!*/ ["bong.mp3", "clock.js", "clock.svg", "index.html", "manifest.json", "moveit.js", "suntimes.js", "sw.js", "tick.mp3", "version.js"]

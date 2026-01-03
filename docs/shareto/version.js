5cfbf3447c48d379c57d7ccfb09099e14c8139bb
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-01-03 16:59:15 +0000

    Add files via upload

--- docs/shareto/version.js
+++ docs/shareto/version.js
@@ -1,3 +1,3 @@
-let cacheName = /*time!*/ "6958d4b0";
+let cacheName = /*time!*/ "69593d39";
 
 let projectFiles = /*files!*/ ["common.js", "destinations.html", "edits.html", "hamburger.svg", "history.html", "index.html", "manifest.json", "shareto.css", "shareto.html", "storage.js", "submit.svg", "sw.js", "version.js"]

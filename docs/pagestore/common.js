e033fdb99fef86b249ad7738c1b9b8b1f1d3207c
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-05-04 18:55:03 +0100

    Add files via upload

--- docs/pagestore/common.js
+++ docs/pagestore/common.js
@@ -25,10 +25,36 @@ if (typeof menu !== "undefined") {
                 const [fileHandle] = await window.showOpenFilePicker();
                 const file = await fileHandle.getFile();
                 const text = await file.text();
-				let imported = JSON.parse(text);
-				console.log(imported);
-				storage.setStorage(imported);  
-                location.reload();              
+                let imported = JSON.parse(text);
+                console.log(imported);
+                storage.setStorage(imported);
+                location.reload();
+            }
+        },
+        {
+            title: "Copy settings", func: async function () {
+                if (menuStorage == undefined) {
+                    throw "Can't backup without data";
+                }
+                let text = menuStorage.toString();
+                navigator.clipboard.writeText(text);
+                window.alert("Settings copied to clipboard");
+            }
+        },
+        {
+            title: "Paste settings", func: async function () {
+                let text = await navigator.clipboard.readText();
+                if (!window.confirm("Paste settings?")) {
+                    return;
+                }
+                try {
+                    let imported = JSON.parse(text);
+                    console.log(imported);
+                    storage.setStorage(imported);
+                    location.reload();
+                } catch (e) {
+                    window.alert("Invalid settings");
+                }
             }
         }
     ];

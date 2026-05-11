6a964e78d7c9546816f000e20531f815b5e47d5b
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-05-11 15:07:36 +0100

    Add files via upload

--- docs/wordgame/version.js
+++ docs/wordgame/version.js
@@ -1,3 +1,3 @@
-let cacheName = /*time!*/ "6a01d8ff";
+let cacheName = /*time!*/ "6a01e230";
 
 let projectFiles = /*files!*/ ["index.html", "stats.html", "sw.js", "version.js", "js/gametypes.js", "js/solutionwords.js", "js/storage.js", "js/validwords.js", "js/wordgame.js", "css/wordgame.css", "images/share.svg"];

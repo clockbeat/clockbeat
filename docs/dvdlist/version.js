0283647985404db5c0577f9bf2ea68fbd70786fa
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-06-03 20:48:01 +0100

    Add files via upload

---
+++ docs/dvdlist/version.js
@@ -0,0 +1,19 @@
+let cacheName = /*time!*/ "6a208329";
+
+let projectFiles = /*files!*/ ["dvdlist.html", "dvdlist.js", "dvdlist.svg", "manifest.json", "sw.js", "version.js"];
+
+
+function stringToColour(string) {
+    let hash = 0;
+    string.split('').forEach(char => {
+        hash = char.charCodeAt(0) + ((hash << 5) - hash);
+    })
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        const value = ((hash >> (i * 8)) & 0xff) | 0x80;
+        colour += value.toString(16).padStart(2, '0');
+    }
+    return colour
+}
+
+let stringForColour = "/pagelist/pagelist.html"

369e0664fbe1efccd55c85b675bcc867a4b0863f
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-06-30 17:59:50 +0100

    Add files via upload

--- docs/pagestore/version.js
+++ docs/pagestore/version.js
@@ -1,5 +1,8 @@
 "use strict"
 
-let cacheName = /*time!*/ "6a43a618";
+let cacheName = /*time!*/ "6a43f29a";
 
 let projectFiles = /*files!*/ ["common.js", "index.html", "manifest.json", "pagestore.css", "pagestore.html", "pagestore.svg", "sw.js", "version.js"];
+
+projectFiles.push("/common/menu2.js");
+projectFiles.push("/common/storage.js");

db229a8fe73d3c32b2499a0ac435eb6323743327
clockbeat <89096549+clockbeat@users.noreply.github.com>
2026-07-16 17:36:20 +0100

    Add files via upload

--- docs/dvdlist/version.js
+++ docs/dvdlist/version.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let cacheName = /*time!*/ "6a4e69f0";
+let cacheName = /*time!*/ "6a5907d6";
 
 let projectFiles = /*files!*/ ["dvdlist.html", "dvdlist.svg", "manifest.json", "sw.js", "version.js"];
 
